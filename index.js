784160a503a8be6f37270770cd27916c8dcda04f
Dominic Tarr <dominic.tarr@gmail.com>
2011-09-09 15:12:54 +1000

    add option to disable recursion

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var search = exports = module.exports = function (files, opts, callback) {
   if(!callback)
     callback = opts, opts = {}
   var prune = opts.prune
+    , maxDepth = opts.recursive === false ? 1 : opts.maxDepth
 
   //given list of files
   //stat each file
@@ -36,7 +37,8 @@ var search = exports = module.exports = function (files, opts, callback) {
     , paths = []
     , stats = []
 
-  function r (files, callback) {
+  function r (files, depth, callback) { //pass in depth
+    //if(!callback) callback = depth, depth = -1
     ctrl.parallel.map(
       ctrl([
         opts.strict != false ? stat : d.tryCatchPass(stat, function (err, cb) {cb(null, null)})
@@ -48,25 +50,27 @@ var search = exports = module.exports = function (files, opts, callback) {
           paths.push(file.path)
           stats.push(file)
 
-          if (file.isDirectory()) 
-            ctrl([ls, r]) (file.path, callback)
+          if (file.isDirectory() && (maxDepth ? depth <= maxDepth : true))
+            ctrl([ls, d.curry(r, 0, depth + 1, -1)]) (file.path, callback)
           else 
             callback(null, file)
         }
       ])
     ) (files, d.curry(callback, 0, paths, stats))
   }
-  r(files, callback)
+  r(files, 0, callback)
 }
 
 if(!module.parent) {
  
   var opts = require('optimist').argv
     , args = opts._.length ? opts._ : process.cwd()
- 
+    , exclude = opts.exclude ? new RegExp(opts.exclude) : /^\.git|node_modules/
+
+  console.log(exclude)
   //ignore git and node
   opts.prune = function filter (file) {
-      return /\/\.git/.exec(file.path) || /node_modules/.exec(file.path)
+      return /*/\.git/.exec(file.name || /node_modules/.exec(file.path) ||*/ (exclude && exclude.exec(file.path))
     }
  
   search(args, opts, function (err, files, stats) {
